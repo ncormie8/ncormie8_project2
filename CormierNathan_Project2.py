80138b7f0cbc1755f3ebf3834bd1e69467cf69e7
ncormie8 <ncormie8@uwo.ca>
2024-11-06 21:45:28 -0500

    Update CormierNathan_Project2.py
    Added given parameters and determined cross-sectional area of the baseball

--- CormierNathan_Project2.py
+++ CormierNathan_Project2.py
@@ -1,6 +1,16 @@
 import numpy as np
 import scipy as sp
 
+# Parameters
+m_bb = 0.145 # baseball mass [kg]
+diam_bb = 0.074 # baseball diameter [m]
+acc_g = -9.81 # gravitational acceleration [m/s^2]
+atmDens = 1.2 # Air density [kg/m^3]
+dragC = 0.35  # drag coefficient
+
+# Computes cross sectional area of the baseball [m^2]
+AreaX_bb = 0.25*np.pi*diam_bb**2
+
 # part 1
 
 # solve the projectile problem by writing a Python function that implements the Euler, Euler-Cromer and Midpoint methods 
@@ -9,4 +19,3 @@ import scipy as sp
 # three methods (Euler, Euler-Cromer or Midpoint) should be used for the integration. It would be useful to be able to turn off 
 # air resistance; handle this with an argument to your function. Your routine should return the horizontal range of the ball (in metres).
 # Be sure to look at Figure 2.4, p33, in your textbook in relation to computing the range.
-
