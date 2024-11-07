b82978528a9cf09e18723ea8d0aaa7477eb039b3
ncormie8 <ncormie8@uwo.ca>
2024-11-06 22:15:28 -0500

    Update CormierNathan_Project2.py
    Rough/initial logical outline for code for part 1

--- CormierNathan_Project2.py
+++ CormierNathan_Project2.py
@@ -7,6 +7,7 @@ diam_bb = 0.074 # baseball diameter [m]
 acc_g = -9.81 # gravitational acceleration [m/s^2]
 atmDens = 1.2 # Air density [kg/m^3]
 dragC = 0.35  # drag coefficient
+hitHeight = 1 # ball is hit from starting height of 1m
 
 # Computes cross sectional area of the baseball [m^2]
 AreaX_bb = 0.25*np.pi*diam_bb**2
@@ -19,3 +20,38 @@ AreaX_bb = 0.25*np.pi*diam_bb**2
 # three methods (Euler, Euler-Cromer or Midpoint) should be used for the integration. It would be useful to be able to turn off 
 # air resistance; handle this with an argument to your function. Your routine should return the horizontal range of the ball (in metres).
 # Be sure to look at Figure 2.4, p33, in your textbook in relation to computing the range.
+
+# Euler method
+
+def projMotion(v_launch, ang_launch, tstep,method,AirResYN):
+
+    r0 = hitHeight
+    v0 = v_launch
+    ang0 = ang_launch
+    Tau = tstep
+
+    if method == 'Euler':
+        # Perform numerical analysis with Euler's Method
+        if AirResYN == 'Yes':
+            # Do calculations with Air resistance
+        else:
+            # Do calculations w/o Air resistance
+    
+    elif method == 'Euler-Cromer':
+        # Perform numerical analysis with Euler-Cromer Method
+        if AirResYN == 'Yes':
+            # Do calculations with Air resistance
+        else:
+            # Do calculations w/o Air resistance
+    
+    
+    elif method == 'Midpoint':
+        # Perform numerical analysis with Midpoint Method
+        
+        if AirResYN == 'Yes':
+            # Do calculations with Air resistance
+        else:
+            # Do calculations w/o Air resistance
+
+    return #array for plotting
+    
