f6f0ec6c7f8708292d8fad778c2fa8174c92f018
ncormie8 <ncormie8@uwo.ca>
2024-11-08 19:45:04 -0500

    adaptation of projMotion for part3
    Modified logic and first returned value of original projMotion, creating projMotionMod which returns the value of height at the first x position which greater than or equal to 400 ft

--- CormierNathan_Project2.py
+++ CormierNathan_Project2.py
@@ -23,7 +23,6 @@ def projMotion(v_launch,ang_launch,tstep,method,AirResYN):
     stepLim = 6000 # massive step limit so I dont run out of position logging space (within reason)
 
     # baseball parameters
-    # Parameters
     m_bb = 0.145 # baseball mass [kg]
     diam_bb = 0.074 # baseball diameter [m]
     acc_g = -9.81 # gravitational acceleration [m/s^2]
@@ -241,5 +240,168 @@ for j in range(randSize):
     if range_out_feet[j] >= hr_distance_ft:
         numHRs += 1
 
+# calculating AB/HR ratio and printing to terminal
 ABHR = randSize/numHRs
 print('AB/HR ratio of RDH: ', np.round(ABHR,2))
+
+
+# Part 3
+
+# Real stadiums have fences and their height varies a lot: for example, the “Green Monster” 
+# left field fence in Boston’s Fenway Park is 11.3 m high, while the outfield fence in London's 
+# Labatt Park is only about 2 m high. Show how the AB/HR-ratio for the RDH varies as fences from 0.5 to 15.0 m 
+# are placed at the 400 foot mark. If you want to keep the AB/HR-ratio greater than 10, how high do 
+# you need to place the fence, or is it always higher than 10 ? (Hint: start by modifying your routine of 
+# Part 1 to instead return the height of the ball at a distance of 400 feet – note that this could be negative).
+
+def projMotionMod(v_launch,ang_launch,tstep,method,AirResYN):
+    '''Takes initial velocity[m/s], launch angle[deg], time step[s], solving method ('Euler' or 'Euler-Cromer' or 'Midpoint'), and air resistance toggle as parameters.
+    Returns the horizonal range of the ball.'''
+
+    ang0 = ang_launch
+    tau = tstep
+    stepLim = 6000 # massive step limit so I dont run out of position logging space (within reason)
+
+    # baseball parameters
+    m_bb = 0.145 # baseball mass [kg]
+    diam_bb = 0.074 # baseball diameter [m]
+    acc_g = -9.81 # gravitational acceleration [m/s^2]
+    atmDens = 1.2 # Air density [kg/m^3]
+    dragC = 0.35  # drag coefficient
+    hitHeight = 1 # ball is hit from starting height of 1m
+
+    # Computes cross sectional area of the baseball [m^2]
+    AreaX_bb = 0.25*np.pi*diam_bb**2
+
+    # calculating the air constant for later use in a formula for cases with air resistance
+    airConst = -0.5*dragC*atmDens*AreaX_bb/m_bb
+
+    # Setting initial position and velocity
+    r0 = [0,hitHeight] #x0, y0
+    v0 = [np.cos(ang0*np.pi/180)*v_launch,np.sin(ang0*np.pi/180)*v_launch] #Vx0, Vy0
+    
+    # Setting initial values of time steppable r and v to initial values
+    r = r0
+    v = v0
+
+    # x initialization for plotting
+    xPos = np.empty(stepLim)
+
+    # y initialization for plotting
+    yPos = np.empty(stepLim)
+    yPos[0] = hitHeight
+
+    ######## Euler method section
+    if method == 'Euler':
+        # Perform numerical analysis with Euler's Method
+        if AirResYN is True:
+            # Do calculations with Air resistance
+            acc = np.zeros(2)
+            i = 1
+            while (r[0]*feet_per_m)<=hr_distance_ft:
+                acc[0] = airConst*abs(v[0])*v[0]           # air resistance (only acc on x)
+                acc[1] = airConst*abs(v[1])*v[1] + acc_g   # air res and gravity (acc on y)
+
+                r[0] = r[0] + tau*v[0]       # Euler's method step for position in x
+                r[1] = r[1] + tau*v[1]       # Euler's method step for position in y
+                v[0] = v[0] + tau*acc[0]     # Euler's method step for velocity in x
+                v[1] = v[1] + tau*acc[1]     # Euler's method step for velocity in y
+                xPos[i] = r[0]    # saving x pos at each step for plotting
+                yPos[i] = r[1]    # saving y pos at each step for plotting
+                i +=1
+
+            return r[1],i-1,xPos,yPos
+        
+        elif AirResYN is False:
+            # Do calculations w/o Air resistance
+            i = 1
+            while (r[0]*feet_per_m)<=hr_distance_ft:
+                r[0] = r[0] + tau*v[0]       # Euler's method step for position in x
+                r[1] = r[1] + tau*v[1]       # Euler's method step for position in y
+                v[1] = v[1] + tau*acc_g      # Euler's method step for velocity in y
+                xPos[i] = r[0]    # saving x pos at each step for plotting
+                yPos[i] = r[1]    # saving y pos at each step for plotting
+                i +=1
+
+            return r[1],i-1,xPos,yPos
+        
+        else:
+            return 'Input Variable for AirResYN was not True or False. Please try again.'
+    
+
+
+    ######## Euler-Cromer method section
+    elif method == 'Euler-Cromer':
+        # Perform numerical analysis with Euler-Cromers Method
+        if AirResYN is True:
+            # Do calculations with Air resistance
+            acc = np.zeros(2)
+            i = 1
+            while (r[0]*feet_per_m)<=hr_distance_ft:
+                acc[0] = airConst*abs(v[0])*v[0]           # air resistance (only acc on x)
+                acc[1] = airConst*abs(v[1])*v[1] + acc_g   # air res and gravity (acc on y)
+
+                v[0] = v[0] + tau*acc[0]     # Euler-Cromer step for velocity in x (same as Euler's)
+                v[1] = v[1] + tau*acc[1]     # Euler-Cromer step for velocity in y (same as Euler's)
+                r[0] = r[0] + tau*v[0]       # Euler-Cromer step for position in x (now using updated vx to find rx)
+                r[1] = r[1] + tau*v[1]       # Euler-Cromer step for position in y (now using updated vy to find ry)
+                xPos[i] = r[0]    # saving x pos at each step for plotting
+                yPos[i] = r[1]    # saving y pos at each step for plotting
+                i +=1
+            
+            return r[1],i-1,xPos,yPos
+        
+        elif AirResYN is False:
+            # Do calculations w/o Air resistance
+            i = 1
+            while (r[0]*feet_per_m)<=hr_distance_ft:
+                v[1] = v[1] + tau*acc_g      # Euler-Cromer step for velocity in y (same as Euler's)
+                r[0] = r[0] + tau*v[0]       # Euler-Cromer step for position in x
+                r[1] = r[1] + tau*v[1]       # Euler-Cromer step for position in y (now using updated vy to find ry)
+                xPos[i] = r[0]    # saving x pos at each step for plotting
+                yPos[i] = r[1]    # saving y pos at each step for plotting
+                i +=1
+
+            return r[1],i-1,xPos,yPos
+        
+        else:
+            return 'Input Variable for AirResYN was not True or False. Please try again.'
+    
+
+
+    ######## Midpoint method section
+    elif method == 'Midpoint':
+        # Perform numerical analysis with Midpoint Method
+        if AirResYN is True:
+            # Do calculations with Air resistance
+            acc = np.zeros(2)
+            i = 1
+            while (r[0]*feet_per_m)<=hr_distance_ft:
+                acc[0] = airConst*abs(v[0])*v[0]                # air resistance (only acc on x)
+                acc[1] = airConst*abs(v[1])*v[1] + acc_g        # air res and gravity (acc on y)
+
+                r[0] = r[0] + tau*v[0] + 0.5*acc[0]*(tau**2)    # Midpoint step for position in x (now using vx and ax to find rx)
+                r[1] = r[1] + tau*v[1] + 0.5*acc[1]*(tau**2)    # Midpoint step for position in y (now using vy and ay to find ry)
+                v[0] = v[0] + tau*acc[0]                        # Midpoint step for velocity in x (same as Euler's)
+                v[1] = v[1] + tau*acc[1]                        # Midpoint step for velocity in y (same as Euler's)
+                xPos[i] = r[0]    # saving x pos at each step for plotting
+                yPos[i] = r[1]    # saving y pos at each step for plotting
+                i +=1
+                
+            return r[1],i-1,xPos,yPos
+        
+        elif AirResYN is False:
+            # Do calculations w/o Air resistance
+            i = 1
+            while (r[0]*feet_per_m)<=hr_distance_ft:
+                r[0] = r[0] + tau*v[0]                        # Midpoint step for position in x (same as Eulers)
+                r[1] = r[1] + tau*v[1] +0.5*acc_g*(tau**2)    # Midpoint step for position in y (now using vy and ay to find ry)
+                v[1] = v[1] + tau*acc_g                       # Midpoint step for velocity in y (same as Euler's)
+                xPos[i] = r[0]    # saving x pos at each step for plotting
+                yPos[i] = r[1]    # saving y pos at each step for plotting
+                i +=1
+                
+            return r[1],i-1,xPos,yPos
+        
+        else:
+            return 'Input Variable for AirResYN was not True or False. Please try again.'
